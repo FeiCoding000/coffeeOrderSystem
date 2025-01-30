fc50615a678bddda25a86abbca2dbf58404231ef
Yafei <weihaianni@gmail.com>
2025-01-30 13:25:28 +1100

    Config formik in menu page instead of dialog

--- coffee-order-system/src/components/orderDialog.jsx
+++ coffee-order-system/src/components/orderDialog.jsx
@@ -5,47 +5,10 @@ import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { useEffect } from "react";
+import { Switch, Slider } from "@mui/material";
 
-export const OrderDialog = ({ open, close, data }) => {
-  console.log("Data received in component:", data);
-
-  const { type, strength, milk, isIced, isClient, isDecaf, isXhot, sugar } =
-    data;
-
-  const formik = useFormik({
-    initialValues: {
-      strength: strength || "",
-      type: type,
-      milk: milk || "",
-      isIced: isIced || false,
-      isDecaf: isDecaf || false,
-      isClient: isClient || false,
-      isXhot: isXhot || false,
-      sugar: sugar || 0,
-    },
-
-    // validationSchema: Yup.object({
-    //   type: Yup.string().required("Required"),
-    //   strength: Yup.string().required("Required"),
-    //   milk: Yup.string().required("Required"),
-    //   isIced: Yup.string().required("Required"),
-    //   isDecaf: Yup.string().required("Required"),
-    //   isClient: Yup.string().required("Required"),
-    //   sugar: Yup.string().required("Required"),
-    // }),
-
-    onSubmit: (values) => {
-      console.log("Submitted Values", values);
-      close();
-    },
-  });
-
-
-
-  console.log("Formik initialValues:", formik.initialValues);
+export const OrderDialog = ({ open, close, type, formik }) => {
+  console.log("Type in component:", type);
 
   return (
     <Dialog open={open} onClose={close}>
@@ -54,13 +17,12 @@ export const OrderDialog = ({ open, close, data }) => {
       {type}
 
       <form onSubmit={formik.handleSubmit}>
-        <TextField
-          // type="hidden"
+        <input
+          type="hidden"
           onChange={formik.handleChange}
           name="type"
           value={type}
-          slotProps={{ readOnly: true }}
-        ></TextField>
+        ></input>
 
         <Select
           onChange={formik.handleChange}
@@ -85,6 +47,57 @@ export const OrderDialog = ({ open, close, data }) => {
           <MenuItem value="Soy">Soy Milk</MenuItem>
           <MenuItem value="Almond">Almond Milk</MenuItem>
         </Select>
+        
+        <Switch
+            name="isIced"
+            value={formik.values.isIced}
+            onChange={formik.handleChange}
+            checked={formik.values.isIced}
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+        >
+        </Switch>
+
+        <Switch
+            name="isDecaf"
+            value={formik.values.isDecaf}
+            onChange={formik.handleChange}
+            checked={formik.values.isDecaf}
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+        >
+        </Switch>
+
+        <Switch
+            name="isExtraHot"
+            value={formik.values.isExtraHot}
+            onChange={formik.handleChange}
+            checked={formik.values.isExtraHot}
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+        >
+        </Switch>
+
+        <Switch
+            name="isClient"
+            value={formik.values.isClient}
+            onChange={formik.handleChange}
+            checked={formik.values.isClient}
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+        >
+        </Switch>
+
+        <Slider
+            name="sugar"
+            value={formik.values.sugar}
+            onChange={formik.handleChange}
+            min={0}
+            max={5}
+            step={1}
+            aria-labelledby="discrete-slider-small-steps"
+            valueLabelDisplay="auto"
+        ></Slider>
 
         <Button type="submit">Submit</Button>
       </form>
--- coffee-order-system/src/pages/menu.jsx
+++ coffee-order-system/src/pages/menu.jsx
@@ -1,86 +1,121 @@
 import { useEffect, useState } from "react";
-import React from 'react'
+import React from "react";
 import { ItemCard } from "../components/itemCard";
 import { OrderDialog } from "../components/orderDialog";
+import { useFormik } from "formik";
+import { Box, Typography } from "@mui/material";
+import * as Yup from "yup";
 
 export const Menu = () => {
-    const defaultData = {
-        type: "",
-        strength:"1",
-        milk:"Full Cream",
-        isIced: false,
-        isClient: false,
-        isDecaf: false,
-        isXhot:false,
-        sugar: 0
-    }
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-    const [drinkData, setDrinkData] = useState(defaultData);
-    //handle drinkData
-    const handlSetDrinkData = () => {
+  //handle dialog open and close
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
 
-    } 
+  //formik setValue to formik.type
+  const handleFormikType = (type) => {
+    formik.setFieldValue("type", type);
+  };
 
-    //handle dialog open and close
-    const handleDialogOpen = () => {
-        setIsDialogOpen(true);
-    }
-    const handleDialogClose = () => {
-        setIsDialogOpen(false);
-    }
+  //set up formik
+  const formik = useFormik({
+    initialValues: {
+      type: "",
+      milk: "Full Cream",
+      strength: "1",
+      sugar: "0",
+      isIced: false,
+      isDecaf: false,
+      isExtraHot: false,
+      isClient: false,
+    },
+    onSubmit: (values) => {
+      console.log("Submitted values:", values);
+      formik.resetForm();
+      handleDialogClose();
+    },
+  });
 
-    const handleSetDrinkType = (input) => {
-        setDrinkData(
-            {
-                ...drinkData,
-                type: input
-            }
-        );
-    }
-
-    useEffect (() => {
-        console.log("data to send, date by default", drinkData, defaultData)
-    }, [drinkData])
-
-    const menuItems= {
-    "coffee":[
-        "Flat White",
-        "Latte",
-        "Long Black"
-    ],
-    "tea": [
-        "English Breakfast",
-        "Earl Gray",
-        "Green Tea"
-    ],
-    "Choc&Chai": [
-        "Choclate",
-        "Chai"
-    ]
-}
-
+  const menuItems = {
+    coffee: ["Flat White", "Latte", "Long Black"],
+    tea: ["English Breakfast", "Earl Gray", "Green Tea"],
+    "Choc&Chai": ["Choclate", "Chai"],
+  };
 
   return (
     <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
         <h1>Menu</h1>
-        <h3>Coffee</h3>
-        {menuItems.coffee.map((item, index) => {
-            return <ItemCard key={index} data={item} onClick={() => {handleSetDrinkType(item); handleDialogOpen()}}></ItemCard>
-        })}
-        
-        <h3>Tea</h3>
-        {menuItems.tea.map((item, index) => {
-            return <ItemCard key={index} data={item} onClick={() => {handleSetDrinkType(item); handleDialogOpen()}}></ItemCard>
-        })}
+        <Typography>Coffee</Typography>
+        <Box sx={{ display: "flex", flexWrap: "wrap" }} gap={2}>
+          {menuItems.coffee.map((item, index) => {
+            return (
+              <ItemCard
+                key={index}
+                data={item}
+                onClick={() => {
+                  handleFormikType(item);
+                  handleDialogOpen();
+                }}
+              ></ItemCard>
+            );
+          })}
+        </Box>
 
-        <h3>Chocolate&Chai</h3>
-        {menuItems["Choc&Chai"].map((item, index) => {
-            return <ItemCard key={index} data={item} onClick={() => {handleSetDrinkType(item); handleDialogOpen()}}></ItemCard>
-        })}
+        <Typography>Tea</Typography>
+        <Box
+          sx={{ display: "flex", flexWrap: "wrap", alignContent: "left" }}
+          gap={2}
+        >
+          {menuItems.tea.map((item, index) => {
+            return (
+              <ItemCard
+                key={index}
+                data={item}
+                onClick={() => {
+                  handleFormikType(item);
+                  handleDialogOpen();
+                }}
+              ></ItemCard>
+            );
+          })}
+        </Box>
 
+        <Typography>Chocolate&Chai</Typography>
+        <Box sx={{ display: "flex", flexWrap: "wrap" }} gap={2}>
+          {menuItems["Choc&Chai"].map((item, index) => {
+            return (
+              <ItemCard
+                key={index}
+                data={item}
+                onClick={() => {
+                  handleFormikType(item);
+                  handleDialogOpen();
+                }}
+              ></ItemCard>
+            );
+          })}
+        </Box>
+      </Box>
 
-        <OrderDialog open={isDialogOpen} close={handleDialogClose} data= {drinkData}></OrderDialog>
+      <OrderDialog
+        open={isDialogOpen}
+        close={handleDialogClose}
+        type={formik.values.type}
+        formik={formik}
+      ></OrderDialog>
     </div>
-  )
-}
+  );
+};
